52d3d17b04c7f977b05433f8a20e72f5f564e6c0
Alumno <alumno@localhost.localdomain>
2014-03-19 09:08:51 -0600

    EXAMEN

--- src/deysi1/exxa1.java
+++ src/deysi1/exxa1.java
@@ -1,10 +1,28 @@
 package deysi1;
 
+import java.util.Scanner;
+
 public class exxa1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		 Scanner sc=new Scanner(System.in);  
+	        System.out.println("Ingresa un numero ");  
+	        int A = sc.nextInt();  
+	        System.out.println("Ingresa otro numero ");  
+	        int B = sc.nextInt();  
+	       
+	        if(A < B ){  
+	        System.out.println("verdadero" + A);  
+	        }else{  
+	            if(A < A ){  
+	            	int A1= sc.nextInt(); 
+	            System.out.println("verdadero a" + B);  
+	            System.out.println("fALSO" + A); 
+	            }else{  
+	            System.out.print("Los numeros son Iguales ");
+		         
+	            }  
+	        }  
+	     }  
+	}  
